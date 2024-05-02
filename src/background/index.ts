0e6b5f1575cdb3a94bbb5092376271d60dbffab1
Kang The Variants <78076796+khengyun@users.noreply.github.com>
2024-05-03 01:15:51 +0700

    fix: wrong share format in instructions page

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,26 +1,36 @@
-console.log('background is running');
-
+// share link
 chrome.webRequest.onSendHeaders.addListener(
   (details) => {
-    const refererHeader = details.requestHeaders?.find((header) => header.name === 'Referer');
-    const referer = refererHeader?.value?.replace('submit', 'review') || '';
-    const url = new URL(details.url);
-    const submissionId = url.searchParams.get('peerSubmissionId')?.split('~')[2] || '';
+    const { requestHeaders = [], url, tabId } = details;
+    const refererHeader = requestHeaders.find?.((header) => header.name === 'Referer');
+    const { value: referer = '' } = refererHeader || {};
+    const { searchParams } = new URL(url);
+    const submissionId = searchParams.get('peerSubmissionId')?.split('~')[2] || '';
+    const refererURL = referer.replace('/submit', '/review')
 
-    if (details.url.includes('onDemandPeer') && submissionId && referer) {
+    if (url.includes('onDemandPeer') && submissionId && refererURL) {
       let submissionLink = '';
-      if (!referer.includes(submissionId)) {
-        submissionLink = `${referer}/${submissionId}`;
-      } else if (!referer.includes("/review")) {
-        submissionLink = `${referer}/review/${submissionId}`;
+
+      switch (true) {
+        case !refererURL.includes(submissionId) && refererURL.includes("/review"):
+          submissionLink = `${refererURL}/${submissionId}`;
+          break;
+        case !refererURL.includes(submissionId) && !refererURL.includes("/review"):
+          submissionLink = `${refererURL}/review/${submissionId}`;
+          break;
+        case !refererURL.includes("/review"):
+          submissionLink = `${refererURL}/review/${submissionId}`;
+          break;
       }
 
       const data: SubmissionType = {
-        Link: submissionLink || referer,
+        Link: submissionLink || refererURL,
       };
 
+      // console.log('Submission Link:', data.Link);
+
       // Send data to the content script of the current tab
-      chrome.tabs.sendMessage(details.tabId, data);
+      chrome.tabs.sendMessage(tabId, data);
     }
   },
   { urls: ['<all_urls>'] },
--- src/contentScript/index.ts
+++ src/contentScript/index.ts
@@ -1,18 +1,26 @@
 chrome.runtime.onMessage.addListener((message: SubmissionType) => {
   if (message.hasOwnProperty('Link')) {
     // Process the data here (e.g., store it or send it elsewhere)
+    // console.log('Submission Link:', message.Link);
 
-    if (document.getElementById('sharelink') == null) {
-      document.getElementById('assignmentTabs')?.insertAdjacentHTML(
-        'beforeend',
-        `
-        <li role="none" id="sharelink" class="colored-tab">
-            <a role="tab" aria-selected="false" class="tab-link" onclick="event.preventDefault(); navigator.clipboard.writeText('${message.Link}').then(() => { alert('Link copied to clipboard!!!'); }).catch((error) => { alert('Failed to copy link to clipboard:', error); });">
-                <span class="tab-text">Share</span>
-            </a>
-        </li>
-        `,
-      )
+    const shareLinkTab = document.getElementById('sharelink');
+    if (!shareLinkTab) {
+      // console.log('Adding share link tab');
+      // Wait 5s before continuing
+      setTimeout(() => {
+        document.getElementById('assignmentTabs')?.insertAdjacentHTML(
+          'beforeend',
+          `
+          <li role="none" id="sharelink" class="colored-tab">
+              <a role="tab" aria-selected="false" class="tab-link" onclick="event.preventDefault(); navigator.clipboard.writeText('${message.Link}').then(() => { alert('Link copied to clipboard!!!'); }).catch((error) => { alert('Failed to copy link to clipboard:', error); });">
+                  <span class="tab-text">Share</span>
+              </a>
+          </li>
+          `
+        );
+      }, 1000);
+    } else {
+      shareLinkTab.querySelector('a')?.setAttribute('onclick', `event.preventDefault(); navigator.clipboard.writeText('${message.Link}').then(() => { alert('Link copied to clipboard!!!'); }).catch((error) => { alert('Failed to copy link to clipboard:', error); });`);
     }
   }
-})
+});
